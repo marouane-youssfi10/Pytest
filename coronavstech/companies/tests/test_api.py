777b6126b9eb850deb0bd5f2eb0b2c62eefbf558
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-04-18 03:56:12 +0000

    testing with Pytest

--- coronavstech/companies/tests/test_api.py
+++ coronavstech/companies/tests/test_api.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 
 from companies.models import Company
 
+companies_url = reverse("companies-list")
 
 @pytest.mark.django_db
 class BasicCompanyAPITestCase(TestCase):
@@ -18,27 +19,23 @@ class BasicCompanyAPITestCase(TestCase):
         pass
 
 @pytest.mark.django_db
-class TestGetCompanies(BasicCompanyAPITestCase):
-    def test_zero_companies_should_return_empty_list(self) -> None:
-        client = Client()
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), [])
+def test_zero_companies_should_return_empty_list(client) -> None:
+    response = client.get(companies_url)
+    assert response.status_code == 200
+    assert (json.loads(response.content)) == []
 
-    def test_one_company_exists_should_succeed(self) -> None:
-        client = Client()
-        test_company = Company.objects.create(name="Amazon")
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
-        response_content = json.loads(response.content)[0]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_content.get("name"), test_company.name)
-        self.assertEqual(response_content.get("application_link"), "")
-        self.assertEqual(response_content.get("notes"), "")
+@pytest.mark.django_db
+def test_one_company_exists_should_succeed(client) -> None:
+    test_company = Company.objects.create(name="Amazon")
+    response = client.get(companies_url)
+    response_content = json.loads(response.content)[0]
+    assert response.status_code == 200
+    assert response_content.get("name") == test_company.name
+    assert response_content.get("application_link") == ""
+    assert response_content.get("notes") == ""
+    test_company.delete()
 
-        test_company.delete()
-
+# -------------------------- test post companies --------------------------------
 class TestPostCompanies(BasicCompanyAPITestCase):
     def test_create_company_without_arguments_should_fail(self) -> None:
         response = self.client.post(path=self.companies_url)
