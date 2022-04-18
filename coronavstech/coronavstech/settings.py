5a7e4f9e64db06c903ee8f64d97b57bdfbd94c29
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-04-18 15:28:11 +0000

    update

--- coronavstech/coronavstech/settings.py
+++ coronavstech/coronavstech/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "ifssouy001@gmail.com"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
